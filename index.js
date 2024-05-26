b839759a49c320236ce94937b98a30fd1c6f0003
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-05-26 16:31:42 +0530

    mongo modify

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect(
-    "mongodb+srv://21955a1206:nikhil1528@project.cyg4qu7.mongodb.net/Travel?retryWrites=true&w=majority&appName=Project"
-)
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://21955a1206:nikhil1528@project.cyg4qu7.mongodb.net/Travel?retryWrites=true&w=majority&appName=Project", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+    
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT ,() => {
     console.log(`Server is running in ${PORT}`);
