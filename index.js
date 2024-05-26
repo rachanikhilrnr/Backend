406399a36464a6df13eaa706f0545d4889a5ebf0
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-05-26 10:53:54 +0530

    mongo

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use(cors({
 }));
 app.use(express.json());
 
+mongoose.connect(
+    "mongodb+srv://21955a1206:nikhil1528@project.cyg4qu7.mongodb.net/Travel?retryWrites=true&w=majority&appName=Project"
+)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT ,() => {
     console.log(`Server is running in ${PORT}`);
