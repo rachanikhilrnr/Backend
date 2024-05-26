9da6234c9229d90d1e2cacb494e7028ab7a81309
rachanikhilrnr <114729903+rachanikhilrnr@users.noreply.github.com>
2024-05-26 17:23:17 +0530

    ASD

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ const cors = require('cors');
 
 const postModel = require('./models/post');
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://21955a1206:nikhil1528@project.cyg4qu7.mongodb.net/Travel?retryWrites=true&w=majority&appName=Project", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-  
 const app = express();
 app.use(cors({
     origin:["","localhost:3000"],
@@ -18,7 +12,11 @@ app.use(cors({
 }));
 app.use(express.json());
 
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://21955a1206:nikhil1528@project.cyg4qu7.mongodb.net/Travel?retryWrites=true&w=majority&appName=Project", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
     
 
 const PORT = process.env.PORT || 5000;
